b4490bb2b0efa058f66cbe9fc21c8de3cd12aa06
Jess White <code.jessw@gmail.com>
2016-01-14 20:54:46 +0000

    Add Home Controller

---
+++ Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace JWeb.Controller
+{
+    public class HomeController : Microsoft.AspNet.Mvc.Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
